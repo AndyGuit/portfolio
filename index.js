162ae08bc243fb74ac829c12db270cc87155f891
AndyGuit <totalslash@msn.com>
2023-02-06 11:12:40 +0300

    fix: burger menu

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 /* Burger Menu */
 const burgerIcon = document.querySelector('.nav__menu-icon');
 const navMenu = document.querySelector('.nav__main');
-const navLink = document.querySelector('.nav__link');
 
 const toggleBurgerMenu = () => {
   burgerIcon.classList.toggle('active');
@@ -9,4 +8,4 @@ const toggleBurgerMenu = () => {
 };
 
 burgerIcon.addEventListener('click', toggleBurgerMenu);
-navLink.addEventListener('click', toggleBurgerMenu);
+navMenu.addEventListener('click', toggleBurgerMenu);
