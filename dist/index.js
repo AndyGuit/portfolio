7087f7f1f892a794068704a8def8be5cb479a962
AndyGuit <totalslash@msn.com>
2024-06-19 01:33:14 +0300

    feat: add no scroll class to body when burger menu is active

--- dist/index.js
+++ dist/index.js
@@ -3,15 +3,18 @@ const burgerIcon = document.querySelector('.nav__menu-icon');
 const navMenu = document.querySelector('.nav__main');
 
 const toggleBurgerMenu = () => {
+  window.scrollTo(0, 0);
   burgerIcon.classList.toggle('active');
   navMenu.classList.toggle('active');
+  document.body.classList.toggle('no-scroll');
 };
 
 burgerIcon.addEventListener('click', toggleBurgerMenu);
 navMenu.addEventListener('click', toggleBurgerMenu);
 
-document.body.addEventListener('click', e => {
+document.body.addEventListener('click', (e) => {
   if (!e.target.closest('.nav__header')) {
+    document.body.classList.remove('no-scroll');
     burgerIcon.classList.remove('active');
     navMenu.classList.remove('active');
   }
