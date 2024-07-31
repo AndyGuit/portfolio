b05f44909f34410f0f31ae3cd05989b38e447600
AndyGuit <totalslash@msn.com>
2024-07-31 23:08:16 +0300

    fix: no scroll on toggling burger menu

--- dist/index.js
+++ dist/index.js
@@ -4,6 +4,7 @@ const navMenu = document.querySelector('.nav__main');
 
 const toggleBurgerMenu = () => {
   window.scrollTo(0, 0);
+  document.body.classList.toggle('no-scroll');
   burgerIcon.classList.toggle('active');
   navMenu.classList.toggle('active');
 };
@@ -12,7 +13,7 @@ burgerIcon.addEventListener('click', toggleBurgerMenu);
 navMenu.addEventListener('click', toggleBurgerMenu);
 
 document.body.addEventListener('click', (e) => {
-  if (!e.target.closest('.nav__header')) {
+  if (!e.target.closest('.nav__header') && navMenu.classList.includes('active')) {
     document.body.classList.remove('no-scroll');
     burgerIcon.classList.remove('active');
     navMenu.classList.remove('active');
